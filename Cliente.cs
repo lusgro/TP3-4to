68521d4a14bcf2e5d5c8631ff78ae1ecd4655c84
lusgro <lucasurielrosenbaum@gmail.com>
2023-04-20 14:15:34 -0300

    correcciones consigna

--- Cliente.cs
+++ Cliente.cs
@@ -1,8 +1,8 @@
 public class Cliente
 {
-   public int dni{get; set;}
-   public string apellido{get; set;}
-   public string nombre{get; set;}
+   public int dni{get; private set;}
+   public string apellido{get; private set;}
+   public string nombre{get; private set;}
    public DateTime fechaInscripcion{get; set;}
    public int tipoEntrada{get; set;}
    public int totalAbonado{get; set;}
